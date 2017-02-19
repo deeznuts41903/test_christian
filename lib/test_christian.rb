cc22c2ec2773ea92e22705c3592613555300cf01
root <root@localhost.localdomain>
2017-02-19 02:49:09 -0500

    first commit

--- lib/test_christian.rb
+++ lib/test_christian.rb
@@ -1,5 +1,15 @@
 require "test_christian/version"
 
 module TestChristian
-  # Your code goes here...
+  class Sys
+ def sys_clear
+   system("clear")
+    end
+ def _n
+  system("echo")
+   end
+ def return_0
+  system("exit 0")
+   end
+end
 end
